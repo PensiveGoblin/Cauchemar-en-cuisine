19bbd0ad4496b8baf36c6593806fc13166db5ea2
mbressieux <mbressieux@student.ethz.ch>
2023-10-06 00:40:28 +0200

    animator sans network

--- Cauchema_en_Cusine/Assets/Scripts/BehaviorPlayer.cs
+++ Cauchema_en_Cusine/Assets/Scripts/BehaviorPlayer.cs
@@ -5,30 +5,63 @@ using Mirror;
 
 public class BehaviorPlayer    
 {
+    public static float velocity=0.0f;
     public static void Motion(PlayerUnit unit)
     {
         
+        {
+            velocity=0.0f;
+            if (Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.LeftShift))  
             {
-                if (Input.GetKey(KeyCode.UpArrow))  
-                {  
-                    unit.transform.Translate(Vector3.forward * Time.deltaTime*PlayerUnit.speed);  
-                }  
-                 
-                if (Input.GetKey(KeyCode.DownArrow))  
-                {  
-                    unit.transform.Translate(Vector3.back * Time.deltaTime);  
-                }  
-                 
-                if (Input.GetKey(KeyCode.LeftArrow))  
-                {  
-                    unit.transform.Rotate(Vector3.up, -10);  
-                }  
-                
-                if (Input.GetKey(KeyCode.RightArrow))  
-                {  
-                    unit.transform.Rotate(Vector3.up, 10);  
+                while (velocity < Time.deltaTime * PlayerUnit.speed)
+                {
+                    velocity+=PlayerUnit.acceleration;
+                }
+                //velocity = Vector3.forward * Time.deltaTime * PlayerUnit.speed;  
+                unit.transform.Translate(Vector3.forward * velocity);  
+            
+            }  
+            else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftShift))
+            {
+                while (velocity < Time.deltaTime * PlayerUnit.speed*2)
+                {
+                    velocity+=PlayerUnit.acceleration;
+                }
+                //velocity = Vector3.forward * Time.deltaTime * PlayerUnit.speed;  
+                unit.transform.Translate(Vector3.forward * velocity);  
+            }
+            else{
+                while (velocity > 0){
+                    velocity-=PlayerUnit.deceleration;
                 }
             }
+                
+            if (Input.GetKey(KeyCode.DownArrow))  
+            {  
+                unit.transform.Translate(Vector3.back * Time.deltaTime);  
+            }  
+                
+            if (Input.GetKey(KeyCode.LeftArrow))  
+            {  
+                unit.transform.Rotate(Vector3.up, -10);  
+            }  
+            
+            if (Input.GetKey(KeyCode.RightArrow))  
+            {  
+                unit.transform.Rotate(Vector3.up, 10);  
+            }
+        }
         
+    }
+
+    public static void Animate(PlayerUnit unit){
+        int VelocityHash;
+        Animator anim;
+        anim=unit.GetComponent<Animator>();
+
+        VelocityHash=Animator.StringToHash("velocity");
+
+        anim.SetFloat(VelocityHash,velocity);
+
     }
 }
--- Cauchema_en_Cusine/Assets/Scripts/PlayerUnit.cs
+++ Cauchema_en_Cusine/Assets/Scripts/PlayerUnit.cs
@@ -6,37 +6,53 @@ namespace Mirror
 {
     public class PlayerUnit : NetworkBehaviour
     {
+        Animator anim;
         //Category
         public string category;
         //Default value
         static public int speed = 5;
         static public int health = 10;
         static public int speedRotation = 2;
+        static public float acceleration = 0.001f;
+        static public float deceleration = 0.05f;
+
+        
         // Start is called before the first frame update
         public override void OnStartLocalPlayer()
             {
                 Camera.main.transform.SetParent(transform);
-                Camera.main.transform.localPosition = new Vector3(0, 0, 0);
+                Camera.main.transform.localPosition = new Vector3(0, 3, -5);
                 Camera.main.transform.rotation = Quaternion.identity;
             }
         void Start()
         {
             if (category == "soldier")
             {
-                speed = 20;
+                speed = 5;
                 health = 100;
                 speedRotation = 2; 
+                acceleration=0.001f;
             }
+            anim=GetComponent<Animator>();
 
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
+            
             if (isLocalPlayer)
             {
                 BehaviorPlayer.Motion(this);
+                BehaviorPlayer.Animate(this);
+                
             }
+            //if(BehaviorPlayer.velocity>0.5){
+            Debug.Log("Velocity: "+BehaviorPlayer.velocity);
+            //}
+            
         }
+
+        
     }
 }
